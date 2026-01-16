e104d867f8d9c2547e5e39073b94a1cc0416c19f
Nicolas Liochon <liochon@gmail.com>
2026-01-16 18:38:35 +0100

    generate positions offline

--- filters.py
+++ filters.py
@@ -46,10 +46,72 @@ def filter_tb_generic(board: chess.Board, tb: Mapping[str, Any]) -> bool:
 
 
 
+# --- filters.py additions for: White KP vs Black K ---
+
+def filter_notb_kp_vs_k(board: chess.Board) -> bool:
+    """
+    KP vs K no-TB specific filter (White to move):
+
+    - The White pawn must be on human rank 3/4/5 (0-based rank 2/3/4).
+    - Reject if the Black king is behind the pawn (bkr < pr) AND the White king does not block the pawn
+      (i.e., White king is not on the square immediately in front of the pawn).
+    """
+    p = next(iter(board.pieces(chess.PAWN, chess.WHITE)))
+    wk = board.king(chess.WHITE)
+    bk = board.king(chess.BLACK)
+
+    pr = chess.square_rank(p)
+    if pr not in (2, 3, 4):
+        return False
+
+    bkr = chess.square_rank(bk)
+
+    # "Block the pawn" = occupy the square directly in front of it (same file, rank+1).
+    # For a white pawn on rank 7 this would be offboard, but rank 7 is excluded by your global pawn rule anyway.
+    pawn_front = p + 8
+
+    if bkr < pr and wk != pawn_front:
+        return False
+
+    return True
 
 
 
+def filter_tb_kp_vs_k(board: chess.Board, tb: Mapping[str, Any]) -> bool:
+    """
+    KP vs K TB-specific filter (White POV outcomes):
 
+    - If White wins (tb["wdl"] == +1): exactly one first move must also be a win.
+    - If draw (tb["wdl"] == 0): require White king to be close to the pawn (Chebyshev distance <= 2).
+    """
+    if tb["wdl"] == 1:
+        winning = 0
+        for m in tb["moves"]:
+            if m["wdl"] == 1:
+                winning += 1
+                if winning > 1:
+                    return False
+        return winning == 1
+
+    if tb["wdl"] == 0:
+        p = next(iter(board.pieces(chess.PAWN, chess.WHITE)))
+        wk = board.king(chess.WHITE)
+
+        pf, pr = chess.square_file(p), chess.square_rank(p)
+        wkf, wkr = chess.square_file(wk), chess.square_rank(wk)
+
+        if max(abs(wkf - pf), abs(wkr - pr)) > 2:
+            return False
+            
+        # White king more than 1 rank behind the pawn => reject.
+        if pr - wkr > 1:
+            return False
+
+    return True
+
+
+
+#######################################################################################""
 
 # --- filters.py additions for: White KR vs Black KP ---
 
